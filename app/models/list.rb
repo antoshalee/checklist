0da2205937a3ac0b929cabc6c80679221cc58776
antoshalee <antoshalee@gmail.com>
2012-11-18 15:22:47 +0800

    new design

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,20 @@
 class List < ActiveRecord::Base
   attr_accessible :name, :records_attributes
   has_many :records
-  accepts_nested_attributes_for :records, allow_destroy: true
+  accepts_nested_attributes_for :records, allow_destroy: true, reject_if: proc { |r| r['content'].blank? }
+
+  validates :name, presence: true
+
+  after_initialize :default_empty_records
+
+  private
+
+  def default_empty_records
+    if new_record? && records.count == 0
+      2.times do
+        records << Record.new
+      end
+    end
+
+  end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,6 @@
 class Record < ActiveRecord::Base
   attr_accessible :content
   belongs_to :list
+
+  validates :content, presence: true
 end
